0a51a1664d75541229032c30fcf152b42bce2490
TMaTTeO99 <113847189+TMaTTeO99@users.noreply.github.com>
2023-04-27 19:19:41 +0200

    Delete WordleClient directory

--- WordleClient/src/ImplementazioneNotificaClient.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.RemoteException;
-import java.rmi.server.RemoteServer;
-
-public class ImplementazioneNotificaClient extends RemoteServer implements NotificaClient {
-    public void SendNotifica(int notifica) throws RemoteException {
-        System.out.println("Tipo di notifica ricevuta: " + notifica);
-        if(notifica == -1)System.out.println("Chiusura del servizio di registrazione");//stampa per indicare cosa succede
-    }
-}
